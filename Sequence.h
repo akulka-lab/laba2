0c823c7b7744c704d0b7a10440f5e95119c6536c
akulka-lab <74815124+akulka-lab@users.noreply.github.com>
2022-06-05 19:29:54 +0300

    Update Sequence.h

--- Sequence.h
+++ Sequence.h
@@ -1,19 +1,23 @@
 #pragma once
+
 using namespace std;
+
 template<typename T>
+
 class Sequence
 {
 public:
-    virtual T GetFirst()                                              const = 0;
-    virtual T GetLast()                                               const = 0;
-    virtual T Get(int index)                                          const = 0;
-    virtual int GetLength()                                           const = 0;
-    virtual void Remove(int index)                                          = 0;
-    virtual void Append(T item)                                             = 0;
-    virtual void Prepend(T item)                                            = 0;
-    virtual void InsertAt(T item,int index)                                 = 0;
-    virtual Sequence<T>* Concat(Sequence<T>* list)                    const = 0;
-    virtual Sequence<T>* GetSubsequence(int startIndex, int endIndex) const = 0;
+    virtual void InsertAt(T item,int index);
+    virtual Sequence<T>* Concat(Sequence<T>* list)const;
+    virtual T Get(int index)const;
+    virtual int GetLength()const;
+    virtual void Remove(int index);
+    virtual void Append(T item);
+    virtual void Prepend(T item);
+     virtual T GetFirst()const;
+    virtual T GetLast() const;
+    
+    virtual Sequence<T>* GetSubsequence(int startIndex, int endIndex) const;
 };
 
 
